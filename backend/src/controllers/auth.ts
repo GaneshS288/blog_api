f175719f54e44c1564d2e30f3f2af3e1e2b0ee72
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-06-01 20:33:46 +0530

    feat: make signup return error when user with same name exists

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 import bcrypt from "bcryptjs";
-import { createUser } from "../db/userQueries.ts";
+import { createUser, findUserByName } from "../db/userQueries.ts";
 
 async function signupUser(req: Request, res: Response) {
     const {
@@ -8,6 +8,15 @@ async function signupUser(req: Request, res: Response) {
         password,
         secretPassword,
     }: { name: string; password: string; secretPassword: string } = req.body;
+
+    const userExists = await findUserByName(name);
+
+    if (userExists) {
+        const status = 400;
+        const errMessage = `A user with name '${name}' already exists`;
+        res.status(400).json({ status, data: { error: errMessage } });
+        return;
+    }
 
     const passwordhash = await bcrypt.hash(password, 12);
 
--- backend/src/tests/authRoute.test.ts
+++ backend/src/tests/authRoute.test.ts
@@ -1,18 +1,26 @@
 import { expect, test, describe, vi } from "vitest";
 import { createRequest, createResponse } from "node-mocks-http";
 import { signupUser } from "../controllers/auth.ts";
-import { createUser } from "../db/userQueries.ts";
+import { createUser, findUserByName } from "../db/userQueries.ts";
 
 // mock the function that calls database to create user
 vi.mock("../db/userQueries.ts", () => {
     return {
-        createUser: vi.fn(), 
-    }
-})
+        createUser: vi.fn(),
+        findUserByName: vi.fn((name) => {
+            const users = [
+                { name: "martin", password: "stall$3000" },
+                { name: "fuwante", password: "drift&3ace" },
+            ];
 
+            const user = users.find((user) => user.name === name);
+            return user;
+        }),
+    };
+});
 
 describe("signup a new user", () => {
-    test("succesfully sign up a user and return 201 status", async () => {
+    test("succesfully signs up a user and return 201 status", async () => {
         const user = { name: "ganesh", password: "blabbityy@345" };
         const req = createRequest({ method: "POST", body: user });
         const res = createResponse();
@@ -21,9 +29,27 @@ describe("signup a new user", () => {
 
         await signupUser(req, res);
 
-        expect(createUser).toHaveBeenCalled()
+        expect(createUser).toHaveBeenCalled();
         expect(res.status).toHaveBeenCalledWith(201);
-        expect(res.json).toHaveBeenCalledWith({status: 201, data: { message: "user successfully created"}})
+        expect(res.json).toHaveBeenCalledWith({
+            status: 201,
+            data: { message: "user successfully created" },
+        });
     });
-    
+
+    test("sends 400 error when a user with same name exist in database", async () => {
+        const user = { name: "martin", password: "stall$3000" };
+        const req = createRequest({ method: "POST", body: user });
+        const res = createResponse();
+        vi.spyOn(res, "status");
+        vi.spyOn(res, "json");
+
+        await signupUser(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({
+            status: 400,
+            data: { error: `A user with name '${user.name}' already exists` },
+        });
+    });
 });
