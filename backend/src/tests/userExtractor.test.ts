a1cc640228413cf0752925a096299906bdcba598
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-06-17 20:53:25 +0530

    feat: add tests for wrong schema and user for user extractor

--- backend/src/tests/userExtractor.test.ts
+++ backend/src/tests/userExtractor.test.ts
@@ -13,7 +13,8 @@ vi.mock("../db/userQueries.ts", () => {
             ];
 
             const user = users.find((user) => user.id === id);
-            return user;
+
+            return user ? user : null;
         }),
     };
 });
@@ -22,12 +23,66 @@ describe("Extracting user from token", () => {
     test("extracts the user and attaches it to reqest", async () => {
         const user = { id: 1, name: "fuwante" };
         const token = jwt.sign(user, JWT_SECRET);
-        const req = createRequest({ headers: { authorization: `Bearer ${token}` } });
+        const req = createRequest({
+            headers: { authorization: `Bearer ${token}` },
+        });
         const res = createResponse();
         const next = vi.fn();
 
         await userExtractor(req, res, next);
 
         expect(req.user.name).toBe(user.name);
+        expect(next).toHaveBeenCalled();
+    });
+
+    test("throws error when authorization header schema is wrong", async () => {
+        const user = { id: 1, name: "fuwante" };
+        const token = jwt.sign(user, JWT_SECRET);
+        const req = createRequest({
+            headers: { authorization: `Common ${token}` },
+        });
+        const res = createResponse();
+        const next = vi.fn();
+
+        try {
+            await userExtractor(req, res, next);
+        } catch (error) {
+            expect(error.status).toBe(401);
+        }
+        expect(next).toHaveBeenCalledTimes(0);
+    });
+
+    test("throws error when token is invalid", async () => {
+        const user = { id: 1, name: "fuwante" };
+        const token = jwt.sign(user, "cats");
+        const req = createRequest({
+            headers: { authorization: `Bearer ${token}` },
+        });
+        const res = createResponse();
+        const next = vi.fn();
+
+        try {
+            await userExtractor(req, res, next);
+        } catch (error) {
+            expect(error.status).toBe(401);
+        }
+        expect(next).toHaveBeenCalledTimes(0);
+    });
+
+    test("throws error when user is invalid", async () => {
+        const user = { id: 4, name: "ganesh" };
+        const token = jwt.sign(user, JWT_SECRET);
+        const req = createRequest({
+            headers: { authorization: `Bearer ${token}` },
+        });
+        const res = createResponse();
+        const next = vi.fn();
+
+        try {
+            await userExtractor(req, res, next);
+        } catch (error) {
+            expect(error.status).toBe(401);
+        }
+        expect(next).toHaveBeenCalledTimes(0);
     });
 });
