da70a8778a4b79d081cecfe7653e99e3d179d299
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2026-01-03 17:55:32 +0530

    fix: add compund id for like tables

--- backend/prisma/migrations/20260103122231_init/migration.sql
+++ backend/prisma/migrations/20260103122231_init/migration.sql
@@ -25,6 +25,14 @@ CREATE TABLE "blogs" (
 );
 
 -- CreateTable
+CREATE TABLE "blog_likes" (
+    "user_id" INTEGER NOT NULL,
+    "blog_id" INTEGER NOT NULL,
+
+    CONSTRAINT "blog_likes_pkey" PRIMARY KEY ("user_id","blog_id")
+);
+
+-- CreateTable
 CREATE TABLE "comments" (
     "id" SERIAL NOT NULL,
     "remote_id" TEXT NOT NULL,
@@ -37,6 +45,14 @@ CREATE TABLE "comments" (
     "parent_comment_id" INTEGER NOT NULL,
 
     CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "comment_likes" (
+    "user_id" INTEGER NOT NULL,
+    "comment_id" INTEGER NOT NULL,
+
+    CONSTRAINT "comment_likes_pkey" PRIMARY KEY ("user_id","comment_id")
 );
 
 -- CreateIndex
@@ -55,6 +71,12 @@ CREATE UNIQUE INDEX "comments_remote_id_key" ON "comments"("remote_id");
 ALTER TABLE "blogs" ADD CONSTRAINT "blogs_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "blog_likes" ADD CONSTRAINT "blog_likes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "blog_likes" ADD CONSTRAINT "blog_likes_blog_id_fkey" FOREIGN KEY ("blog_id") REFERENCES "blogs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "comments" ADD CONSTRAINT "comments_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -62,3 +84,9 @@ ALTER TABLE "comments" ADD CONSTRAINT "comments_blog_id_fkey" FOREIGN KEY ("blog
 
 -- AddForeignKey
 ALTER TABLE "comments" ADD CONSTRAINT "comments_parent_comment_id_fkey" FOREIGN KEY ("parent_comment_id") REFERENCES "comments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment_likes" ADD CONSTRAINT "comment_likes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment_likes" ADD CONSTRAINT "comment_likes_comment_id_fkey" FOREIGN KEY ("comment_id") REFERENCES "comments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
