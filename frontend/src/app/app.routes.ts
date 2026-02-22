aa3cdfaf1d11ab9c3aada105c3b668aed42d34e0
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2026-02-22 15:10:33 +0530

    add signup page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Signup } from './pages/signup/signup';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Homepage' },
+  {
+    path: 'signup',
+    component: Signup,
+    title: 'Signup',
+  },
+];
